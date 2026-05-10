a23601e96c97ef2c3fc91520a9be9804952493ed
Vatsal Solanki <solanki.vatsal009@gmail.com>
2026-05-10 20:32:55 +0530

    updated: app.py

--- app.py
+++ app.py
@@ -3,46 +3,104 @@ import mysql.connector
 
 app = Flask(__name__)
 
-# MySQL connection function
 def get_db_connection():
-    connection = mysql.connector.connect(
+    return mysql.connector.connect(
         host="mysql-container",
         user="root",
         password="rootvatsal",
         database="quotesdb"
     )
-    return connection
 
-# Home route
 @app.route("/")
 def home():
-    try:
-        db = get_db_connection()
+    return jsonify({
+        "message": "Two tier Flask MySQL app running",
+        "status": "success"
+    })
 
-        return jsonify({
-            "message": "Flask app is running",
-            "database": "MySQL Connected Successfully",
-            "status": "success"
-        })
-
-    except Exception as e:
-        return jsonify({
-            "message": "Database connection failed",
-            "error": str(e),
-            "status": "failed"
-        }), 500
-
-# Health check route
 @app.route("/health")
 def health():
     return jsonify({
         "status": "healthy"
-    }), 200
+    })
 
-# Additional test route
-@app.route("/hello")
-def hello():
-    return "Hello from Flask CI/CD two tier project!"
+@app.route("/advice")
+def advice():
+
+    db = get_db_connection()
+
+    cursor = db.cursor()
+
+    query = """
+    SELECT message FROM quotes
+    WHERE category='advice'
+    ORDER BY RAND()
+    LIMIT 1
+    """
+
+    cursor.execute(query)
+
+    result = cursor.fetchone()
+
+    cursor.close()
+    db.close()
+
+    return jsonify({
+        "category": "advice",
+        "message": result[0]
+    })
+
+@app.route("/quote")
+def quote():
+
+    db = get_db_connection()
+
+    cursor = db.cursor()
+
+    query = """
+    SELECT message FROM quotes
+    WHERE category='quote'
+    ORDER BY RAND()
+    LIMIT 1
+    """
+
+    cursor.execute(query)
+
+    result = cursor.fetchone()
+
+    cursor.close()
+    db.close()
+
+    return jsonify({
+        "category": "quote",
+        "message": result[0]
+    })
+
+@app.route("/joke")
+def joke():
+
+    db = get_db_connection()
+
+    cursor = db.cursor()
+
+    query = """
+    SELECT message FROM quotes
+    WHERE category='joke'
+    ORDER BY RAND()
+    LIMIT 1
+    """
+
+    cursor.execute(query)
+
+    result = cursor.fetchone()
+
+    cursor.close()
+    db.close()
+
+    return jsonify({
+        "category": "joke",
+        "message": result[0]
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
