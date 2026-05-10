fe9bd051bb00670207bb229605485d1eeec5b8f5
Vatsal Solanki <solanki.vatsal009@gmail.com>
2026-05-10 20:27:56 +0530

    updated: app.py

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
 from flask import Flask, jsonify
+import mysql.connector
 
 app = Flask(__name__)
+
+# MySQL connection function
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host="mysql-container",
+        user="root",
+        password="rootvatsal",
+        database="quotesdb"
+    )
+    return connection
 
 # Home route
 @app.route("/")
 def home():
-    return jsonify({
-        "message": "Flask app is running ",
-        "status": "success"
-    })
+    try:
+        db = get_db_connection()
 
-# Health check route (important for DevOps)
+        return jsonify({
+            "message": "Flask app is running",
+            "database": "MySQL Connected Successfully",
+            "status": "success"
+        })
+
+    except Exception as e:
+        return jsonify({
+            "message": "Database connection failed",
+            "error": str(e),
+            "status": "failed"
+        }), 500
+
+# Health check route
 @app.route("/health")
 def health():
     return jsonify({
@@ -20,7 +42,7 @@ def health():
 # Additional test route
 @app.route("/hello")
 def hello():
-    return "Hello from Flask CI/CD project!"
+    return "Hello from Flask CI/CD two tier project!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- dockerfile
+++ dockerfile
@@ -2,8 +2,9 @@ FROM ubuntu
 
 RUN apt-get update
 RUN apt-get install -y python3 
+RUN apt install -y python3-pip
 RUN apt-get install -y python3-flask
-RUN pip3 install mysql-connector-python --break-system-packages
+RUN pip install mysql-connector-python --break-system-packages
 
 COPY . /opt/
 
